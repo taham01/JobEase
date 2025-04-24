511a282dac8a5b017aaa4d72cd81ecb7b6ead102
taham01 <mhmdd.taha9@hotmail.com>
2025-04-24 22:35:48 +0300

    test: log webhook hit for debugging

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -6,55 +6,61 @@ export const clerkWebhooks = async (req, res) => {
   console.log("Webhook hit!", req.body);
   try {
     // Create a Svix instance with clerk webhook secret.
-    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
-    console.log("Webhook hit (unverified)", req.body);
+    export const clerkWebhooks = async (req, res) => {
+      console.log("🚀 Webhook HIT!");
+      console.log("BODY:", req.body);
+      res.status(200).json({ success: true });
+    };
+    
+  //   const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+  //   console.log("Webhook hit (unverified)", req.body);
 
-    // Verifying Headers
-    // await whook.verify(JSON.stringify(req.body), {
-    //   "svix-id": req.headers["svix-id"],
-    //   "svix-timestamp": req.headers["svix-timestamp"],
-    //   "svix-signature": req.headers["svix-signature"],
-    // });
+  //   // Verifying Headers
+  //   // await whook.verify(JSON.stringify(req.body), {
+  //   //   "svix-id": req.headers["svix-id"],
+  //   //   "svix-timestamp": req.headers["svix-timestamp"],
+  //   //   "svix-signature": req.headers["svix-signature"],
+  //   // });
 
-    // Getting Data from request body
-    const { data, type } = req.body;
+  //   // Getting Data from request body
+  //   const { data, type } = req.body;
 
-    // Switch Cases for differernt Events
-    switch (type) {
-      case "user.created": {
-        const userData = {
-          _id: data.id,
-          email: data.email_addresses[0].email_address,
-          name: data.first_name + " " + data.last_name,
-          image: data.image_url,
-          resume: "",
-        };
-        await User.create(userData);
-        res.json({});
-        break;
-      }
+  //   // Switch Cases for differernt Events
+  //   switch (type) {
+  //     case "user.created": {
+  //       const userData = {
+  //         _id: data.id,
+  //         email: data.email_addresses[0].email_address,
+  //         name: data.first_name + " " + data.last_name,
+  //         image: data.image_url,
+  //         resume: "",
+  //       };
+  //       await User.create(userData);
+  //       res.json({});
+  //       break;
+  //     }
 
-      case "user.updated": {
-        const userData = {
-          email: data.email_addresses[0].email_address,
-          name: data.first_name + " " + data.last_name,
-          image: data.image_url,
-        };
-        await User.findByIdAndUpdate(data.id, userData);
-        res.json({});
-        break;
-      }
+  //     case "user.updated": {
+  //       const userData = {
+  //         email: data.email_addresses[0].email_address,
+  //         name: data.first_name + " " + data.last_name,
+  //         image: data.image_url,
+  //       };
+  //       await User.findByIdAndUpdate(data.id, userData);
+  //       res.json({});
+  //       break;
+  //     }
 
-      case "user.deleted": {
-        await User.findByIdAndDelete(data.id);
-        res.json({});
-        break;
-      }
-      default:
-        break;
-    }
-  } catch (error) {
-    console.log(error.message);
-    res.json({ success: false, message: "Webhooks Error" });
-  }
+  //     case "user.deleted": {
+  //       await User.findByIdAndDelete(data.id);
+  //       res.json({});
+  //       break;
+  //     }
+  //     default:
+  //       break;
+  //   }
+  // } catch (error) {
+  //   console.log(error.message);
+  //   res.json({ success: false, message: "Webhooks Error" });
+  // }
 };
