4b4ebb70ca53cb18c3da7414c5169badfa5996a3
taham01 <mhmdd.taha9@hotmail.com>
2025-04-24 22:16:07 +0300

    Update webhooks controller

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -3,16 +3,17 @@ import User from "../models/User.js";
 
 // API Controller Function to Manage Clerk User with database
 export const clerkWebhooks = async (req, res) => {
+  console.log("Webhook hit!", req.body);
   try {
     // Create a Svix instance with clerk webhook secret.
     const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
     // Verifying Headers
-    await whook.verify(JSON.stringify(req.body), {
-      "svix-id": req.headers["svix-id"],
-      "svix-timestamp": req.headers["svix-timestamp"],
-      "svix-signature": req.headers["svix-signature"],
-    });
+    // await whook.verify(JSON.stringify(req.body), {
+    //   "svix-id": req.headers["svix-id"],
+    //   "svix-timestamp": req.headers["svix-timestamp"],
+    //   "svix-signature": req.headers["svix-signature"],
+    // });
 
     // Getting Data from request body
     const { data, type } = req.body;
